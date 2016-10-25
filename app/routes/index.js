28df57287c527b94260ad6936a815f7bfea41810
Ethan Law and Katrina Hirsch <ethan@>
2016-10-25 13:54:15 -0700

    add functionality to delete a record

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,10 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('article');
   },
+  actions: {
+    destroyArticle(article) {
+      article.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'bustle-clone',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
